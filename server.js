9597c8761608251dd9a47816faaa7664032c7c7e
mn-48 <nazmul.cse48@gmail.com>
2024-01-11 12:47:11 +0600

    client js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname+"/index.html")
 })
 
+app.use(express.static('public'));
+
 io.on('connection', function(socket){
     console.log(`Client is connected: ${socket.id}`);
 })
